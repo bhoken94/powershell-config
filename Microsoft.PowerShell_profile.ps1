a13dc6d230f6ca75048b4851008fc22db161fe4b
santomassimo <alessandra.santomassimo@innovery.net>
2022-01-19 17:54:35 +0100

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿Set-PoshPrompt -Theme C:\Users\Alessandra\Documents\WindowsPowerShell\Modules\oh-my-posh\themes\material.omp.ale.json
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+# New-Alias <alias> <aliased-command>
+New-Alias open ii
